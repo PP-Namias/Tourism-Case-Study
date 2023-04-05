4f84340dc9566f806231356d20ec2fa44f364688
Jhon Keneth Namias <75213409+PP-Namias@users.noreply.github.com>
2023-04-05 23:30:52 +0800

    Card prototype

--- js/main.js
+++ js/main.js
@@ -109,3 +109,36 @@ $(document).ready(function() {
   };
 
 });
+
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+const screen = document.querySelector(".screen"),
+      name = document.querySelector(".name");
+
+screen.onmouseenter = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    name.innerText = name.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return name.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= name.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
